1d92ffa56586f65877409e90b8e0bb44526065f9
Krzysztof_R <romanowski.krzysiu@gmail.com>
2017-05-19 03:26:20 +0200

    + shifted and rotated functions but there are some bugs

--- zso.py
+++ zso.py
@@ -4,13 +4,12 @@ from math import *
 import random
 from statistics import variance
 
-dimensionsNum = 1
+dimensionsNum = 10
 searchRange = 10 # could be use to initialization only or to making walk range in iterations (dependent on knowledge about a position of best fitness or not)
 thresholdVal = 2.5 # could be dependent on average fitness
 HordeSize = 100
 WalkingDeadSpeed = 0.05 # could be dependent on searchRange
 ApocalipseIteration = 100
-
 
 # main function
 def main():
@@ -18,8 +17,26 @@ def main():
     global thresholdVal, HordeSize, WalkingDeadSpeed
     zombieHorde = initZombies(HordeSize)
 
-    print("'fun' function:")
-    bestFitness, bestZombie = executeZSO(zombieHorde, fun, thresholdVal, WalkingDeadSpeed, ApocalipseIteration)
+    o1, M1 = loadShiftAndRotationData("shuffle_data_1_D10.txt", "M_1_D10.txt")
+    # print(o1)
+    # print(M1)
+    o3, M3 = loadShiftAndRotationData("shuffle_data_3_D10.txt", "M_3_D10.txt")
+    # print(o3)
+    # print(M3)
+    o4, M4 = loadShiftAndRotationData("shuffle_data_4_D10.txt", "M_4_D10.txt")
+    # print(o4)
+    # print(M4)
+    o5, M5 = loadShiftAndRotationData("shuffle_data_5_D10.txt", "M_5_D10.txt")
+    # print(o5)
+    # print(M5)
+
+    # print("'fun' function:")
+    # bestFitness, bestZombie = executeZSO(zombieHorde, fun, thresholdVal, WalkingDeadSpeed, ApocalipseIteration)
+    # print("Best fitness: ", bestFitness)
+    # print("Location: ", bestZombie['location'])
+
+    print("'shifted and rotated bent cigar' function:")
+    bestFitness, bestZombie = executeZSO(zombieHorde, shifted_and_rotated_bent_cigar, o1, M1, thresholdVal, WalkingDeadSpeed, ApocalipseIteration)
     print("Best fitness: ", bestFitness)
     print("Location: ", bestZombie['location'])
 
@@ -28,32 +45,32 @@ def fun(x):
     return sqrt(0.5*abs(x))*cos(x)+sqrt(abs(x))
 
 # basic functions
-def bent_cigar(*xs):
+def bent_cigar(xs):
     return x[0]**2 + (10**6)*sum(tuple((x**2 for x in xs[1:])))
 
-def rosenbrock(*xs):
+def rosenbrock(xs):
     return sum(tuple(100*(x[i]**2-x[i+1])**2 + (x[i]-1)**2 for i in range(len(xs)-1)))
 
-def rastrigin(*xs):
+def rastrigin(xs):
     return sum(tuple(x[i]**2 - 10*cos(2*pi*x[i]) + 10 for i in range(len(xs)))) 
 
-def zakharov(*xs):
+def zakharov(xs):
     return sum(tuple(x**2 for x in xs)) + (0.5*sum(tuple(x for x in xs)))**2 + (0.5*sum(tuple(x for x in xs)))**4
 
 # shifted and rotated functions
-def shifted_and_rotated_bent_cigar(*xs): # No. 1, optimum = 100
-    pass
+def shifted_and_rotated_bent_cigar(xs, os, M, F_best=100): # No. 1, optimum = 100
+    return bent_cigar(rotateFunc(shiftFunc(xs, os), M)) + F_best
 
-def shifted_and_rotated_rosenbrock(*xs): # No. 4, optimum = 400
-    pass
+def shifted_and_rotated_rosenbrock(xs, os, M, F_best=400): # No. 4, optimum = 400
+    return rosenbrock(tuple(map(lambda y: y+1, rotateFunc(tuple(map(lambda x: 0.02048*x, shiftFunc(xs, os), M)))))) + F_best
 
-def shifted_and_rotated_rastrigin(*xs): # No. 5, optimum = 500
-    pass
+def shifted_and_rotated_rastrigin(xs, os, M, F_best=500): # No. 5, optimum = 500
+    return bent_rastrigin(rotateFunc(shiftFunc(xs, os), M)) + F_best
 
-def shifted_and_rotated_zakharov(*xs): # No. 3, optimum = 300
-    pass
+def shifted_and_rotated_zakharov(xs, os, M, F_best=300): # No. 3, optimum = 300
+    return zakharov(rotateFunc(shiftFunc(xs, os), M)) + F_best
 
-def shiftFunction(x_list, o_list): # return shifted x list, o_list - shifted global optimum list
+def shiftFunc(x_list, o_list): # return shifted x list, o_list - shifted global optimum list
     new_x_list = []
     for i in range(len(x_list)):
         new_x_list.append(x_list[i]-o_list[i])
@@ -63,9 +80,20 @@ def rotateFunc(x_list, M): # return rotated x list, M - rotation matrix
     new_x_list = [0 for _ in range(len(x_list))]
     for i in range(len(x_list)):
         for j in range(len(x_list)):
+            print(x_list[j])
             new_x_list[i] += x_list[j]*M[i*len(x_list)+j]
     return new_x_list
 
+def loadShiftAndRotationData(shiftFilePath, rotationFilePath):
+    with open(shiftFilePath) as sf, open(rotationFilePath) as rf:
+        o_list = [float(s) for s in sf.readline().split("	")]
+        r_matrix = list(
+            map(
+                lambda str_row: list(map(lambda s: float(s), str_row)), 
+                tuple(map(lambda row_str: tuple(filter(lambda s: s!='', row_str.split(" "))), rf.readlines()))
+            ))
+    return o_list, r_matrix
+	
 # zombie horde initialization
 def initZombies(n): # initialize n zombies
 
@@ -87,11 +115,11 @@ def distance(locA, locB):
     return sqrt(sum(tuple((locB[i]-locA[i])**2 for i in range(len(locA)))))
 
 # algorithm's execution
-def executeZSO(zombiesVec, fitnessFunc, thresholdVal, speed, generationsNum):
+def executeZSO(zombiesVec, fitnessFunc, os, M, thresholdVal, speed, generationsNum):
 
     # zombies hunt for humans
     global dimensionsNum
-    bestFitness = fitnessFunc(*zombiesVec[0]['location'])
+    bestFitness = fitnessFunc(zombiesVec[0]['location'], os, M)
     bestZombie = None
 
     for _ in range(generationsNum):
@@ -103,7 +131,7 @@ def executeZSO(zombiesVec, fitnessFunc, thresholdVal, speed, generationsNum):
             for i in range(dimensionsNum):
                 zombie["location"][i] += zombie["direction"][i]*dirVariance[i]*speed
 
-            fitnessVal = fitnessFunc(*zombie["location"])
+            fitnessVal = fitnessFunc(zombie["location"], os, M)
             bestFitness = fitnessVal if fitnessVal < bestFitness else bestFitness
             bestZombie = zombie if fitnessVal == bestFitness else bestZombie
 
